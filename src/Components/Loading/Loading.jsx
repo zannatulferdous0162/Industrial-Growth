5a9e0ba68317ddccd022ebcc44746a4ba1ca1ccb
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-04-28 21:33:08 +0600

    change loading

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
     return (
         <div className='flex justify-center mt-[200px] h-screen'>
       <div>
-        <div class="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-6000"></div>
+        <div class="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
       </div>
         </div>
     );
